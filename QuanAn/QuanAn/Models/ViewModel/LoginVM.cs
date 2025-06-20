43f50c44c231c84da47dd0f9599af388ee09a4d6
AMKKT07111006 <lengophuckhang158@gmail.com>
2025-06-20 10:06:30 +0700

    Signed-off-by: Test Signed-off-by: AMKKT07111006 <lengophuckhang158@gmail.com>

---
+++ QuanAn/QuanAn/Models/ViewModel/LoginVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QuanAn.Models.ViewModel
+{
+    public class LoginVM
+    {
+        [Required]
+        [Display(Name = "Tên Đăng nhập")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mật khẩu")]
+        public string Password { get; set; }
+    }
+}
