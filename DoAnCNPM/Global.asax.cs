2aa9d43678dbb683b46e611dd4549fa6538c3c0d
AMKKT07111006 <lengophuckhang158@gmail.com>
2025-07-30 21:29:59 +0700

    them trang table

--- DoAnCNPM/Global.asax.cs
+++ DoAnCNPM/Global.asax.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
-namespace DoAnCNPM
+namespace QuanAn
 {
     public class MvcApplication : System.Web.HttpApplication
     {
---
+++ DoAnCNPM/Models/ViewModel/OrderBillManagementVM.cs
@@ -0,0 +1,11 @@
+﻿// Models/ViewModel/OrderBillManagementVM.cs
+using System.Collections.Generic;
+
+namespace QuanAn.Models.ViewModel
+{
+    public class OrderBillManagementVM
+    {
+        public IEnumerable<OrderListVM> Orders { get; set; } // Sử dụng OrderListVM để có thông tin chính của Order
+        public IEnumerable<OrderItemVM> OrderDetails { get; set; } // Tất cả chi tiết các món ăn của các Order
+    }
+}
